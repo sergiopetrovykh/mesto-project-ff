312251b5008dd4f009af8a45793da3cd7e350c5e
Сергей Петровых <sergio-p@yandex.ru>
2024-06-16 22:44:19 +0500

    Поменял карточки

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Усьвинские столбы",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_65b53bf2bb12e30bc0b25886_65b53c4528512c617ca3b2f8/scale_1200",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Голубые озера в Александровске",
+      link: "https://www.perm.kp.ru/share/i/12/10507404/",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Каменный город",
+      link: "https://laguna-kurort.ru/wp-content/uploads/c/9/3/c936f30ce765619ec6d401c2adf112cf.jpeg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Маньпупунёр",
+      link: "https://tourpedia.ru/wp-content/uploads/2017/04/%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9-%D0%9C%D0%B0%D0%BD%D1%8C%D0%BF%D1%83%D0%BF%D1%83%D0%BD%D0%B5%D1%80.jpeg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Верхнечусовские городки",
+      link: "https://static.guidego.ru/63d38dfa5eeb6bb0ca77c8bc.1600x1200.jpeg",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Вишера",
+      link: "https://klau.club/uploads/posts/2023-02/1677500703_klau-club-p-rasteniya-i-zhivotnie-reki-vishera-vkontak-15.jpg",
     }
 ];
